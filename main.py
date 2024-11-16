67fe0efee0289798b8e530b01f1443856c465103
User <sobaka@sobaka.sobaka>
2024-11-16 19:58:48 +0300

    a

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 import traceback
 
+
 def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
     marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
                               [marker_size / 2, marker_size / 2, 0],
@@ -20,19 +21,20 @@ def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
         trash.append(nada)
     return rvecs, tvecs, trash
 
+
 tello = Tello()
 tello.connect()
-print(f"Battery: {tello.get_battery()}%\nTemp: {tello.get_temperature()}°C") #Вывод заряда и температуры дрона
+print(f"Battery: {tello.get_battery()}%\nTemp: {tello.get_temperature()}°C")  # Вывод заряда и температуры дрона
 markers = int(input("Кол-во объектов: "))
-scan = int(input("Скорость сканирования: "))
-markerSizeInCM = 22 #Установка размера маркера
-with open('calibration.yaml') as f: #Загрузка матрицы искривления объектива камеры дрона
+scan = 30
+markerSizeInCM = 22  # Установка размера маркера
+with open('calibration.yaml') as f:  # Загрузка матрицы искривления объектива камеры дрона
     loadeddict = yaml.safe_load(f)
 mtx = loadeddict.get('camera_matrix')
 dist = loadeddict.get('dist_coeff')
 mtx = np.array(mtx)
 dist = np.array(dist)
-tello.streamon() #Включение камеры
+tello.streamon()  # Включение камеры
 frame_read = tello.get_frame_read()
 try:
     tello.send_rc_control(0, 0, 0, 0)
@@ -56,26 +58,33 @@ try:
                 cv2.imshow("Image", image)
                 cv2.waitKey(1)
                 time.sleep(2)
-                
+
                 image = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
                 cv2.imshow("Image", image)
                 cv2.waitKey(1)
                 (corners, _, _) = detector.detectMarkers(image)
                 rvec, tvec, _ = my_estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
-                tello.go_xyz_speed(int(tvec[0][0][2]) - 100, int(0 - tvec[0][0][0]), int(0 - tvec[0][0][1]) - 30, 50)
-                
+                tello.go_xyz_speed(int(tvec[0][2]) - 100, int(0 - tvec[0][0]),
+                                   int(0 - tvec[0][1] - 30), 50)
+                rotate = int(rvec[0][0])
+                if rotate > 0:
+                    tello.rotate_clockwise(rotate)
+                elif rotate < 0:
+                    tello.rotate_counter_clockwise(0 - rotate)
+
                 image = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
                 cv2.imshow("Image", image)
                 cv2.waitKey(1)
                 (corners, _, _) = detector.detectMarkers(image)
                 rvec, tvec, _ = my_estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
-                tello.go_xyz_speed(int(tvec[0][0][2]) - 30, int(0 - tvec[0][0][0]), int(0 - tvec[0][0][1]) - 30, 50)
-                rotate = int(rvec[0][0][0])
+                tello.go_xyz_speed(int(tvec[0][2]) - 40, int(0 - tvec[0][0]),
+                                   int(0 - tvec[0][1] - 30), 20)
+                rotate = int(rvec[0][0])
+                if rotate > 0:
+                    tello.rotate_clockwise(rotate)
+                elif rotate < 0:
+                    tello.rotate_counter_clockwise(0 - rotate)
                 break
-    if rotate > 0:
-        tello.rotate_clockwise(rotate + 180)
-    elif rotate < 0:
-        tello.rotate_counter_clockwise(0 - (180 + rotate))
 except Exception:
     print(traceback.format_exc())
     tello.send_rc_control(0, 0, 0, 0)
