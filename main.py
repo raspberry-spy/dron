0c9c6fd163731014804060e1eba169f4e5e2a5b9
User <sobaka@sobaka.sobaka>
2024-11-16 13:58:04 +0300

    a

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import numpy as np
 import time
 import traceback
 
+def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
+    marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
+                              [marker_size / 2, marker_size / 2, 0],
+                              [marker_size / 2, -marker_size / 2, 0],
+                              [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
+    trash = []
+    rvecs = []
+    tvecs = []
+    for c in corners:
+        nada, R, t = cv2.solvePnP(marker_points, c, mtx, distortion, None, None, False, cv2.SOLVEPNP_IPPE_SQUARE)
+        rvecs.append(R)
+        tvecs.append(t)
+        trash.append(nada)
+    return rvecs, tvecs, trash
+
 tello = Tello()
 tello.connect()
 print(f"Battery: {tello.get_battery()}%\nTemp: {tello.get_temperature()}°C") #Вывод заряда и температуры дрона
@@ -25,8 +40,8 @@ try:
     image = frame_read.frame
     dimensions = image.shape
     print(dimensions)
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-    arucoParams = cv2.aruco.DetectorParameters_create()
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    arucoParams = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
     for i in range(markers):
         tello.send_rc_control(0, 0, 0, scan)
@@ -34,7 +49,7 @@ try:
             image = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
             cv2.imshow("Image", image)
             cv2.waitKey(1)
-            (corners, ids, rejected) = detector.detectMarkers(image, arucoDict, parameters=arucoParams)
+            (corners, ids, _) = detector.detectMarkers(image)
             if len(corners) > 0 and i in ids and len(ids) == 1:
                 tello.send_rc_control(0, 0, 0, 0)
                 print("Found marker")
@@ -45,15 +60,15 @@ try:
                 image = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
                 cv2.imshow("Image", image)
                 cv2.waitKey(1)
-                (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
-                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
+                (corners, _, _) = detector.detectMarkers(image)
+                rvec, tvec, _ = my_estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
                 tello.go_xyz_speed(int(tvec[0][0][2]) - 100, int(0 - tvec[0][0][0]), int(0 - tvec[0][0][1]) - 30, 50)
                 
                 image = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
                 cv2.imshow("Image", image)
                 cv2.waitKey(1)
-                (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
-                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
+                (corners, _, _) = detector.detectMarkers(image)
+                rvec, tvec, _ = my_estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
                 tello.go_xyz_speed(int(tvec[0][0][2]) - 30, int(0 - tvec[0][0][0]), int(0 - tvec[0][0][1]) - 30, 50)
                 rotate = int(rvec[0][0][0])
                 break
